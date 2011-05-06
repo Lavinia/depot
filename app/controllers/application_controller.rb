fb12fc01520103042834e44affb3d1ceafd5adb0
Lavinia <forgetmenotox@notingham.se>
2011-05-06 11:45:42 +0200

    cart creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  
+    def current_cart    #only available to controllers
+      Cart.find(session[:cart_id])  #gets the cart-id from the session object and tries to find a corresponding card.
+    rescue ActiveRecord::RecordNotFound #if not found
+      cart = Cart.create    #create new cart.
+      session[:cart_id] = cart.id       #save cart.id in session.
+      cart            #return new cart.
+    end
+    
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title' # show in alphabetic order.
+  has_many :line_items    #each product may have many line-items referencing it.
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01} 
   validates :title, :uniqueness => true, :length => { :minimum => 10, :message => "Too short!"}
@@ -8,4 +11,15 @@ class Product < ActiveRecord::Base
     :message => "Must be a URL for GIF, JPG or PNG image."
   }
   
+  private
+  
+    #ensure that there are no line items referencing this product. Hook-method = method thats being called at a given point in an object´s life. Here before we destroy a row in db.
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
